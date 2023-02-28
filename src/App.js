28c6b17f4f5615c1dcadc10fa10917a39fa6b856
64010051 <64010051@kmitl.ac.th>
2023-02-28 15:41:13 +0700

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,33 +1,14 @@
 // import logo from './logo.svg';
 import './App.css';
-import Click from "./Components/Button.js";
-// function App() {
-//   return (
-//     <div className="App">
-//       <header className="App-header">
-//         <img src={logo} className="App-logo" alt="logo" />
-//         <p>
-//           Edit <code>src/App.js</code> and save to reload.
-//         </p>
-//         <a
-//           className="App-link"
-//           href="https://reactjs.org"
-//           target="_blank"
-//           rel="noopener noreferrer"
-//         >
-//           Learn React
-//         </a>
-//       </header>
-//     </div>
-//   );
-// }
+import Savmeal from "./Components/savory_food.js";
+
 
 function App() {
   return(
-    <div className ="App">
+    <div>
       <header className="App-header">
-        <h1>โหวตอาหาร</h1>
-        <Click/>
+      <h1>โหวตอาหาร</h1>
+        <Savmeal/>
       </header>
     </div>
     
